1161d8fe141d470c7a8acc806282b2deb5fcb79c
xiehao123qq <1523823709@qq.com>
2024-10-15 11:54:15 +0800

    网络异常捕获，ma请求优化，请求间隔修改
    网络异常捕获，ma请求优化，请求间隔修改

--- ma.py
+++ ma.py
@@ -48,6 +48,6 @@ def ma(zhouqi,parent=None):
             print(f"请求失败，状态码: {response.status_code}, 错误信息: {response.text}")
     except requests.exceptions.RequestException as e:
         wx.MessageBox("请求超时，请检查网络连接或代理是否正常配置。", "网络错误", wx.OK | wx.ICON_ERROR, parent)
-        print(1)
+        print(11)
 
 #aaa=ma(30)
