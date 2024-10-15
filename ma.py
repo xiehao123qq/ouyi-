90a5f28948057dff3f397d7d857527f0e0bb72b6
xiehao123qq <1523823709@qq.com>
2024-10-15 20:26:23 +0800

    优化
    优化

--- ma.py
+++ ma.py
@@ -1,6 +1,32 @@
+import threading
+from tkinter import Tk, Button, Label
+
 import requests
 import pandas as pd
 import wx
+import os
+os.environ["http_proxy"] = "http://127.0.0.1:7897"
+os.environ["https_proxy"] = "http://127.0.0.1:7897"
+def show_custom_message1(message):
+    root = Tk()
+    root.title("提醒")
+
+    # 设置窗口的大小为 600x600
+    root.geometry("600x600")
+
+    # 设置标签背景，并通过 grid() 布局填充整个窗口
+    label = Label(root, text=message, font=("Arial", 30, "bold"), fg="blue", bg="lightyellow")
+    label.grid(row=0, column=0, sticky="nsew")  # 让标签充满整个窗口区域
+
+    # 配置行和列的权重，使它们可以自动扩展
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+
+    # OK按钮
+    ok_button = Button(root, text="OK", command=root.destroy)
+    ok_button.grid(row=1, column=0, pady=20)
+
+    root.mainloop()
 
 def ma(zhouqi,parent=None):
     base_url = 'https://www.okx.com/api/v5/market/candles'
@@ -41,13 +67,16 @@ def ma(zhouqi,parent=None):
         # 输出时间、收盘价和MA30
            # print(df[['ts', 'close', 'MA']].tail(1))
             ma=df['MA'].tail(1).values[0]
-            #print(ma)
+           # print(ma)
             return ma
 
         else:
             print(f"请求失败，状态码: {response.status_code}, 错误信息: {response.text}")
+            return None
     except requests.exceptions.RequestException as e:
-        wx.MessageBox("请求超时，请检查网络连接或代理是否正常配置。", "网络错误", wx.OK | wx.ICON_ERROR, parent)
-        print(1)
-
-aaa=ma(120)
+        mes="请求超时，"+"\n\n\n"+"请检查网络连接"+"\n\n"+"或代理是否正常配置"
+        #wx.MessageBox("请求超时，请检查网络连接或代理是否正常配置。", "网络错误", wx.OK | wx.ICON_ERROR, parent)
+        threading.Thread(target=show_custom_message1, args=(mes,)).start()
+        print(e)
+        return None
+#aaa=ma(120)
--- mainGui.py
+++ mainGui.py
@@ -1,10 +1,10 @@
 import subprocess
-
+import threading
+from tkinter import Tk, Label, Button
 import wx.xrc
 from ma import ma
 import gettext
 from datetime import datetime
-import os
 _ = gettext.gettext
 
 ###########################################################################
@@ -35,9 +35,50 @@ _ = gettext.gettext
 import threading
 import time
 # 主线程结束，守护线程自动退出
+def show_custom_message1(message):
+    root = Tk()
+    root.title("提醒")
 
+    # 设置窗口的大小为 600x600
+    root.geometry("600x600")
+
+    # 设置标签背景，并通过 grid() 布局填充整个窗口
+    label = Label(root, text=message, font=("Arial", 30, "bold"), fg="blue", bg="lightyellow")
+    label.grid(row=0, column=0, sticky="nsew")  # 让标签充满整个窗口区域
+
+    # 配置行和列的权重，使它们可以自动扩展
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+
+    # OK按钮
+    ok_button = Button(root, text="OK", command=root.destroy)
+    ok_button.grid(row=1, column=0, pady=20)
+
+    root.mainloop()
+
+def show_custom_message2(message):
+    root = Tk()
+    root.title("信息")
+
+    # 设置窗口的大小为 600x600
+    root.geometry("600x600")
+
+    # 设置标签背景，并通过 grid() 布局填充整个窗口
+    label = Label(root, text=message, font=("Arial", 30, "bold"), fg="blue", bg="lightyellow")
+    label.grid(row=0, column=0, sticky="nsew")  # 让标签充满整个窗口区域
+
+    # 配置行和列的权重，使它们可以自动扩展
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+
+    # OK按钮
+    ok_button = Button(root, text="OK", command=root.destroy)
+    ok_button.grid(row=1, column=0, pady=20)
+
+    root.mainloop()
 class MyFrame1 ( wx.Frame ):
     def __init__( self, parent ):
+
         wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.num1 = None
@@ -184,125 +225,6 @@ class MyFrame1 ( wx.Frame ):
         self.m_button7.Bind(wx.EVT_BUTTON, self.start_func)
         self.m_button6.Bind(wx.EVT_BUTTON, self.qqStarting)
         self.m_button71.Bind(wx.EVT_BUTTON, self.stopqq)
-        # Connect Events
-        # self.m_button3.Bind( wx.EVT_BUTTON, self.qqStarting )
-        # self.m_button71.Bind(wx.EVT_BUTTON, self.stopqq)
-    # def __init__( self, parent ):
-    #     #wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-    #     wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-    #     # 创建 Panel
-    #     self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-    #     self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
-    #     self.num1=0
-    #     self.num2=0
-    #     bSizer1 = wx.BoxSizer( wx.VERTICAL )
-    #
-    #     self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, _(u"欢迎使用易欧交易监控程序"), wx.DefaultPosition, wx.Size( 500,-1 ), wx.ALIGN_CENTER_HORIZONTAL )
-    #     self.m_staticText5.Wrap( -1 )
-    #
-    #     self.m_staticText5.SetFont( wx.Font( 16, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, "宋体" ) )
-    #     self.m_staticText5.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_HIGHLIGHT ) )
-    #     self.m_staticText5.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
-    #
-    #     bSizer1.Add( self.m_staticText5, 0, wx.ALL, 5 )
-    #
-    #     bSizer3 = wx.BoxSizer( wx.VERTICAL )
-    #
-    #     self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 500,-1 ), 0 )
-    #     self.m_staticText9.Wrap( -1 )
-    #
-    #     bSizer3.Add( self.m_staticText9, 0, wx.ALL, 5 )
-    #
-    #     fgSizer1 = wx.FlexGridSizer( 0, 2, 0, 0 )
-    #     fgSizer1.SetFlexibleDirection( wx.BOTH )
-    #     fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-    #
-    #     self.m_staticText11 = wx.StaticText( self, wx.ID_ANY, _(u"请输入要计算的差值的垂直距离，如，-6~6"), wx.DefaultPosition, wx.Size( 250,-1 ), 0 )
-    #     self.m_staticText11.Wrap( -1 )
-    #
-    #     fgSizer1.Add( self.m_staticText11, 0, wx.ALL, 5 )
-    #
-    #     fgSizer3 = wx.FlexGridSizer( 0, 2, 0, 0 )
-    #     fgSizer3.SetFlexibleDirection( wx.BOTH )
-    #     fgSizer3.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-    #
-    #     self.m_textCtrl7 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-    #     fgSizer3.Add( self.m_textCtrl7, 0, wx.ALL, 5 )
-    #
-    #     self.m_button2 = wx.Button( self,wx.ID_ANY, _(u"开始运行"), wx.DefaultPosition, wx.DefaultSize, 0 )
-    #     fgSizer3.Add( self.m_button2, 0, wx.ALL, 5 )
-    #
-    #
-    #     fgSizer1.Add( fgSizer3, 1, wx.EXPAND, 5 )
-    #
-    #     bSizer5 = wx.BoxSizer( wx.VERTICAL )
-    #
-    #     self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-    #     self.m_staticText8.Wrap( -1 )
-    #
-    #     bSizer5.Add( self.m_staticText8, 0, wx.ALL, 5 )
-    #
-    #     self.m_staticText91 = wx.StaticText( self, wx.ID_ANY, _(u"停止"), wx.DefaultPosition, wx.Size( 250,-1 ), wx.ALIGN_CENTER_HORIZONTAL )
-    #     self.m_staticText91.Wrap( -1 )
-    #
-    #     bSizer5.Add( self.m_staticText91, 0, wx.ALL, 5 )
-    #
-    #
-    #     fgSizer1.Add( bSizer5, 1, wx.EXPAND, 5 )
-    #
-    #     fgSizer5 = wx.FlexGridSizer( 0, 2, 0, 0 )
-    #     fgSizer5.SetFlexibleDirection( wx.BOTH )
-    #     fgSizer5.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-    #
-    #     bSizer9 = wx.BoxSizer( wx.VERTICAL )
-    #
-    #     self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-    #     self.m_staticText10.Wrap( -1 )
-    #
-    #     bSizer9.Add( self.m_staticText10, 0, wx.ALL, 5 )
-    #
-    #     self.m_button7 = wx.Button( self, wx.ID_ANY, _(u"停止程序"), wx.DefaultPosition, wx.Size( 125,-1 ), 0 )
-    #     bSizer9.Add( self.m_button7, 0, wx.ALL, 5 )
-    #     self.m_button7.Disable()
-    #
-    #     fgSizer5.Add( bSizer9, 1, wx.EXPAND, 5 )
-    #
-    #
-    #     fgSizer1.Add( fgSizer5, 1, wx.EXPAND, 5 )
-    #
-    #
-    #     bSizer3.Add( fgSizer1, 1, wx.EXPAND, 5 )
-    #
-    #     bSizer7 = wx.BoxSizer( wx.VERTICAL )
-    #
-    #     fgSizer4 = wx.FlexGridSizer( 0, 2, 0, 0 )
-    #     fgSizer4.SetFlexibleDirection( wx.BOTH )
-    #     fgSizer4.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-    #
-    #     self.m_staticText38 = wx.StaticText( self, wx.ID_ANY, _(u"结果"), wx.DefaultPosition, wx.Size( 250,-1 ), wx.ALIGN_CENTER_HORIZONTAL )
-    #     self.m_staticText38.Wrap( -1 )
-    #
-    #     fgSizer4.Add( self.m_staticText38, 0, wx.ALL, 5 )
-    #
-    #     self.m_textCtrl8 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
-    #     fgSizer4.Add( self.m_textCtrl8, 0, wx.ALL, 5 )
-    #
-    #
-    #     bSizer7.Add( fgSizer4, 1, wx.EXPAND, 5 )
-    #
-    #
-    #     bSizer3.Add( bSizer7, 1, wx.EXPAND, 5 )
-    #
-    #
-    #     bSizer1.Add( bSizer3, 1, wx.EXPAND, 5 )
-    #
-    #
-    #     self.SetSizer( bSizer1 )
-    #     self.Layout()
-    #
-    #     self.Centre( wx.BOTH )
-
-        # Connect Events
         self.m_button2.Bind( wx.EVT_BUTTON, self.find_square)
         self.m_button7.Bind( wx.EVT_BUTTON, self.start_func )
 
@@ -311,6 +233,7 @@ class MyFrame1 ( wx.Frame ):
         self.worker_thread = None
         self.running1 = False
         self.worker_thread1 = None
+
     def __del__( self ):
         pass
     def start(self):
@@ -319,20 +242,34 @@ class MyFrame1 ( wx.Frame ):
         num2=self.num2
         while self.running:
             # 进行计算
-            result=str(ma(30) - ma(120))
-            self.m_textCtrl8.SetValue(result)
-            print(f"计算的结果: {result}")
-            time1=datetime.now()
-            print("现在的时间是"+str(time1))
-            if int(ma(30) - ma(120)) <= num2 and int(ma(30) - ma(120)) >= num1:
-                current_time = datetime.now()
-                wx.MessageBox(f"符合预期，现在的时间是{current_time}", "提示", wx.OK | wx.ICON_INFORMATION)
-            # 等待 60 秒，但每次只 sleep 1 秒，检查 stop 信号
-            for _ in range(60):
-                if not self.running:
-                    print("任务提前停止")
-                    return
-                time.sleep(1)
+            a=ma(30)
+            b=ma(120)
+            if a is not None and b is not None:
+                c=a-b
+                result=str(c)
+                self.m_textCtrl8.SetValue(result)
+                print(f"计算的结果: {result}")
+                time1=datetime.now()
+                print("现在的时间是"+str(time1))
+                if int(c) <= num2 and int(c) >= num1:
+                    current_time = str(datetime.now())
+                    formatted_time = current_time[:10] + "\n\n" + current_time[10:19]+"\n\n"+result+"\n"  # 手动换行
+                    message2=f"符合预期，\n\n现在的时间是\n\n{formatted_time}"
+                    threading.Thread(target=show_custom_message2, args=(message2,)).start()
+                    # wx.MessageBox(f"符合预期，现在的时间是{current_time}", "提示", wx.OK | wx.ICON_INFORMATION)
+                # 等待 60 秒，但每次只 sleep 1 秒，检查 stop 信号
+                for _ in range(10):
+                    if not self.running:
+                        print("任务提前停止")
+                        return
+                    time.sleep(1)
+            else:
+                print("计算失败，可能是数据获取或计算错误")
+                for _ in range(10):
+                     if not self.running:
+                         print("任务提前停止")
+                         return
+                     time.sleep(1)
 
         print("任务完成")
 
@@ -349,7 +286,8 @@ class MyFrame1 ( wx.Frame ):
         print(f"第一个数字: {self.num1}")
         print(f"第二个数字: {self.num2}")
         if self.num2 < self.num1:
-            wx.MessageBox(f"设置有误请重新输入", "提示", wx.OK | wx.ICON_INFORMATION)
+            message_err="设置有误请重新输入"+"\n\n\n\n\n\n"
+            show_custom_message1(message=message_err)
             event.Skip()
         else:
             print(self.num2-self.num1)
@@ -360,7 +298,8 @@ class MyFrame1 ( wx.Frame ):
             self.worker_thread.start()
             self.m_button2.Disable()  # 启动后禁用 Start 按钮
             self.m_button7.Enable()    # 启动后启用 Stop 按钮
-            wx.MessageBox(f"程序已开始运行", "提示", wx.OK | wx.ICON_INFORMATION)
+            message="程序开始运行"+"\n\n\n\n\n\n"
+            show_custom_message1(message=message)
 
     def start_func(self, event):
         if self.running:
@@ -368,7 +307,8 @@ class MyFrame1 ( wx.Frame ):
             self.worker_thread.join()    # 等待线程结束
             self.m_button2.Enable()   # 停止后启用 Start 按钮
             self.m_button7.Disable()   # 停止后禁用 Stop 按钮
-            wx.MessageBox(f"程序已停止运行", "提示", wx.OK | wx.ICON_INFORMATION)
+            message = "程序停止运行" + "\n\n\n\n\n\n"
+            show_custom_message1(message=message)
     def qqStarting( self, event ):
         if not self.running1:
             self.running1 = True
